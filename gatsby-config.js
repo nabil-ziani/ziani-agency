719d7cc70fdd147229c7359b55d6cd948b53c0af
Nabil_Ziani <s100721@ap.be>
2020-10-28 22:35:20 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,22 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "WPGraphql",
+        fieldName: "wpcontent",
+        url: "http://artist-agency.codobi.be/graphql",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Roboto`, `Oswald`],
+        display: "swap",
+      },
+    },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
